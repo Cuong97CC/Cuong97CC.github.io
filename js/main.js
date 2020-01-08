487cd7938b084a40708198361ed5267b25df7e13
cuong97cc <osident1997@gmail.com>
2020-01-08 16:45:00 +0700

    add some effect

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(document).on("click", ".nav-bar-toggle", function() {
   $(".sidebar").toggleClass("open");
 })
+
 $(document).on("click", ".hide-sidebar", function() {
   $(".sidebar").removeClass("open");
+})
+
+$(document).on("click", ".section-name", function() {
+  var content = $(this).closest(".section").find(".section-content");
+  if ($(content).is(":visible")) $(content).slideUp();
+  else $(content).slideDown();
+})
+
+$(document).on("click", ".nav-item", function() {
+  if ($(this).data("target")) {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $($(this).data("target")).offset().top
+    }, 500)
+  }
+})
+
+$(document).on("click", ".sidebar-item", function() {
+  $(".sidebar").removeClass("open");
+  if ($(this).data("target")) {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $($(this).data("target")).offset().top
+    }, 500)
+  }
 })
